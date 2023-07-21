d56544b57186bf13eb84a6abef365da8940bf0f0
DanGraziano <dangraziano@outlook.com>
2023-07-20 22:34:48 -0400

    Add workout screens

---
+++ app/src/main/java/edu/northeastern/rhythmrun/ActiveWorkout.java
@@ -0,0 +1,14 @@
+package edu.northeastern.rhythmrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActiveWorkout extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_active_workout);
+	}
+}

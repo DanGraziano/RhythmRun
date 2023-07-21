b6f79f1c474909d384a126e1dc6237a6e448e3c7
DanGraziano <dangraziano@outlook.com>
2023-07-20 23:28:55 -0400

    Rename begin workout activity to start workout

--- app/src/main/java/edu/northeastern/rhythmrun/StartWorkout.java
+++ app/src/main/java/edu/northeastern/rhythmrun/StartWorkout.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BeginWorkout extends AppCompatActivity {
+public class StartWorkout extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_begin_workout);
+		setContentView(R.layout.activity_start_workout);
 	}
 }

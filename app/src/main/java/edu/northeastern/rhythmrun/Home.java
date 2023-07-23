a25549193252f390ae2725ecad3481191c1a76e9
DanGraziano <dangraziano@outlook.com>
2023-07-22 23:47:34 -0400

    Rename history to home

---
+++ app/src/main/java/edu/northeastern/rhythmrun/Home.java
@@ -0,0 +1,56 @@
+package edu.northeastern.rhythmrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Home extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_history);
+
+
+
+
+
+
+
+
+
+		// KEEP -- Used for floating action button on click
+		FloatingActionButton fabButton = findViewById(R.id.startWorkoutFab);
+		fabButton.setOnClickListener(v -> {
+			// Open the Workout activity when FAB is clicked
+			Intent intent = new Intent(Home.this, StartWorkout.class);
+			startActivity(intent);
+		});
+
+
+		// KEEP -- Used for bottom navigation bar on click
+		BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+		bottomNavigationView.setOnItemSelectedListener(
+				(item) -> {
+					// Open Home screen on click
+					if (item.getItemId() == R.id.homeNavBar) {
+						Intent homeIntent = new Intent(Home.this, Home.class);
+						startActivity(homeIntent);
+					}
+					// Open Setting screen on click
+					else if (item.getItemId() == R.id.settingsNavBar) {
+						Intent settingsIntent = new Intent(Home.this, Settings.class);
+						startActivity(settingsIntent);
+					}
+					return false;
+				}
+		);
+		// KEEP -- Used for bottom navigation bar appearance and functionality
+		bottomNavigationView.setBackground(null); // Set background to null to make it transparent
+		bottomNavigationView.getMenu().getItem(1).setEnabled(false); // Disable the second menu item
+	}
+}

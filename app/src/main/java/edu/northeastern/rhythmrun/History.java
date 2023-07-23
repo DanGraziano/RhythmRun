8d6cd8e8b7b5aa0e5c5f02b232840c2502d52060
DanGraziano <dangraziano@outlook.com>
2023-07-22 23:01:45 -0400

    Add bottom navigation bar

--- app/src/main/java/edu/northeastern/rhythmrun/History.java
+++ app/src/main/java/edu/northeastern/rhythmrun/History.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class History extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_history);
+
+
+		// KEEP -- Used for bottom navigation bar
+		BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+		bottomNavigationView.setBackground(null); // Set background to null to make it transparent
+		bottomNavigationView.getMenu().getItem(1).setEnabled(false); // Disable the second menu item
 	}
 }

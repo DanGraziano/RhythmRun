653828653ad6b8559bcd5a5defec6136870995fe
DanGraziano <dangraziano@outlook.com>
2023-07-22 23:47:27 -0400

    Add bottom nav bar to start workout, post workout, and settings

--- app/src/main/java/edu/northeastern/rhythmrun/PostWorkout.java
+++ app/src/main/java/edu/northeastern/rhythmrun/PostWorkout.java
@@ -2,7 +2,11 @@ package edu.northeastern.rhythmrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PostWorkout extends AppCompatActivity {
 
@@ -10,5 +14,50 @@ public class PostWorkout extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post_workout);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// KEEP -- Used for floating action button on click
+		FloatingActionButton fabButton = findViewById(R.id.startWorkoutFab);
+		fabButton.setOnClickListener(v -> {
+			// Open the Workout activity when FAB is clicked
+			Intent intent = new Intent(PostWorkout.this, StartWorkout.class);
+			startActivity(intent);
+		});
+
+
+		// KEEP -- Used for bottom navigation bar on click
+		BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+		bottomNavigationView.setOnItemSelectedListener(
+				(item) -> {
+					// Open Home screen on click
+					if (item.getItemId() == R.id.homeNavBar) {
+						Intent homeIntent = new Intent(PostWorkout.this, Home.class);
+						startActivity(homeIntent);
+					}
+					// Open Setting screen on click
+					else if (item.getItemId() == R.id.settingsNavBar) {
+						Intent settingsIntent = new Intent(PostWorkout.this, Settings.class);
+						startActivity(settingsIntent);
+					}
+					return false;
+				}
+		);
+		// KEEP -- Used for bottom navigation bar appearance and functionality
+		bottomNavigationView.setBackground(null); // Set background to null to make it transparent
+		bottomNavigationView.getMenu().getItem(1).setEnabled(false); // Disable the second menu item
 	}
 }
--- app/src/main/java/edu/northeastern/rhythmrun/Settings.java
+++ app/src/main/java/edu/northeastern/rhythmrun/Settings.java
@@ -2,7 +2,11 @@ package edu.northeastern.rhythmrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class Settings extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+
+
+
+
+
+
+
+
+
+
+
+		// KEEP -- Used for floating action button on click
+		FloatingActionButton fabButton = findViewById(R.id.startWorkoutFab);
+		fabButton.setOnClickListener(v -> {
+			// Open the Workout activity when FAB is clicked
+			Intent intent = new Intent(Settings.this, StartWorkout.class);
+			startActivity(intent);
+		});
+
+		// KEEP -- Used for bottom navigation bar on click
+		BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+		bottomNavigationView.setOnItemSelectedListener(
+				(item) -> {
+					// Open Home screen on click
+					if (item.getItemId() == R.id.homeNavBar) {
+						Intent homeIntent = new Intent(Settings.this, Home.class);
+						startActivity(homeIntent);
+					}
+					// Open Setting screen on click
+					else if (item.getItemId() == R.id.settingsNavBar) {
+						Intent settingsIntent = new Intent(Settings.this, Settings.class);
+						startActivity(settingsIntent);
+					}
+					return false;
+				}
+		);
+		// KEEP -- Used for bottom navigation bar appearance and functionality
+		bottomNavigationView.setBackground(null); // Set background to null to make it transparent
+		bottomNavigationView.getMenu().getItem(1).setEnabled(false); // Disable the second menu item
 	}
 }

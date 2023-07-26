0f7b1124e1052b985481cc3e501f9d0242f0dca4
kylefairbairn <kyle.fairbairn@gmail.com>
2023-07-26 16:26:31 -0400

    in progress

--- app/src/main/java/edu/northeastern/rhythmrun/Settings.java
+++ app/src/main/java/edu/northeastern/rhythmrun/Settings.java
@@ -4,25 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
+
+
+	private Button logout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 
+		logout = findViewById(R.id.logoutbtn);
 
-
-
-
-
-
-
-
+		logout.setOnClickListener(v -> {
+			FirebaseAuth.getInstance().signOut();
+			Intent intent = new Intent(Settings.this, Login.class);
+			startActivity(intent);
+		});
 
 
 		// KEEP -- Used for floating action button on click

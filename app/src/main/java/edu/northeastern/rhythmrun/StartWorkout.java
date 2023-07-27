a895a79b88ab7d7fea8fd68e4e9614511d2eb4ee
Dave Centeno <centeno.d@northeastern.edu>
2023-07-26 21:49:33 -0400

    started adding in location services into startworkout activity

--- app/src/main/java/edu/northeastern/rhythmrun/StartWorkout.java
+++ app/src/main/java/edu/northeastern/rhythmrun/StartWorkout.java
@@ -1,27 +1,60 @@
 package edu.northeastern.rhythmrun;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+
+
 public class StartWorkout extends AppCompatActivity {
+
+	private static final int RECORD_REQUEST_CODE = 101;
+	FusedLocationProviderClient fusedLocationProviderClient;
+	LocationRequest locationRequest;
+
+	private LocationCallback locationCallBack;
+
+	Location startingLocation = new Location("");
+
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_workout);
 
+		//Location Services Client
+		fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
+		//Create locationRequest settings
+		locationRequest = LocationRequest.create()
+				.setInterval(10000)
+				.setFastestInterval(5000)
+				.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+				.setMaxWaitTime(15000);
 
+		//Callback for requestLocationUpdates
+		locationCallBack = new LocationCallback() {
+			@Override
+			public void onLocationResult(@NonNull LocationResult locationResult) {
+				super.onLocationResult(locationResult);
 
+			}
+		};
 
-
-
-
+		//Buttons and OnClickListeners
 		// KEEP -- Used for floating action button on click
 		FloatingActionButton fabButton = findViewById(R.id.startWorkoutFab);
 		fabButton.setOnClickListener(v -> {
@@ -29,7 +62,6 @@ public class StartWorkout extends AppCompatActivity {
 			Intent intent = new Intent(StartWorkout.this, StartWorkout.class);
 			startActivity(intent);
 		});
-
 
 		// KEEP -- Used for bottom navigation bar on click
 		BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -52,4 +84,7 @@ public class StartWorkout extends AppCompatActivity {
 		bottomNavigationView.setBackground(null); // Set background to null to make it transparent
 		bottomNavigationView.getMenu().getItem(1).setEnabled(false); // Disable the second menu item
 	}
+
+
 }
+

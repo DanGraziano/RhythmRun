d62f0193185836ce0c6316a51187de41b687a81d
DanGraziano <dangraziano@outlook.com>
2023-08-14 12:47:51 -0400

    Create post workout screen to show run details

--- app/src/main/java/edu/northeastern/rhythmrun/PostWorkout.java
+++ app/src/main/java/edu/northeastern/rhythmrun/PostWorkout.java
@@ -1,28 +1,81 @@
 package edu.northeastern.rhythmrun;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PostWorkout extends AppCompatActivity {
+	private TextView distanceData;
+	private TextView cadenceData;
+	private TextView timeData;
+	private TextView paceData;
+	private TextView dateData;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post_workout);
 
+		// Initialize your TextViews
+		distanceData = findViewById(R.id.distanceData);
+		cadenceData = findViewById(R.id.cadenceData);
+		dateData = findViewById(R.id.dateData);
+		paceData = findViewById(R.id.paceData);
+		timeData = findViewById(R.id.timeData);
 
+		FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
+		if (currentUser != null) {
+			String userUid = currentUser.getUid();
+			DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+					.child(userUid)
+					.child("Runs");
 
+			// Retrieve the run ID from the intent
+			String runId = getIntent().getStringExtra("runId");
 
+			// Retrieve the specific run data using the run ID
+			DatabaseReference specificRunRef = databaseReference.child(runId);
 
+			specificRunRef.addListenerForSingleValueEvent(new ValueEventListener() {
+				@Override
+				public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+					if (dataSnapshot.exists()) {
+						// Fetch the run details from the snapshot
+						String date = String.valueOf(dataSnapshot.child("date").getValue());
+						String distance = dataSnapshot.child("distance").getValue() + " mi";
+						String avgCadence = dataSnapshot.child("avgCadence").getValue() + " spm";
+						String time = String.valueOf(dataSnapshot.child("time").getValue());
+						String avgPace = dataSnapshot.child("avgPace").getValue() + " /mi";
 
+						// Populate your TextViews with retrieved data
+						distanceData.setText(distance);
+						cadenceData.setText(avgCadence);
+						timeData.setText(time);
+						paceData.setText(avgPace);
+						dateData.setText(date);
+					}
+				}
 
-
+				@Override
+				public void onCancelled(@NonNull DatabaseError databaseError) {
+					// Handle errors if needed
+				}
+			});
+		}
 
 
 
